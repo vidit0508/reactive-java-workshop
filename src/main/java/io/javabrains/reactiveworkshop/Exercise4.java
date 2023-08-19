32a22bd65661a48644289bc0033981558ce09151
a13400880 <Qwerty@1234>
2023-08-19 20:32:18 +0530

    committing Exercise4.

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
@@ -10,12 +11,23 @@ public class Exercise4 {
 
         // Print the value from intNumberMono when it emits
         // TODO: Write code here
+        // ReactiveSources.intNumberMono().subscribe(number -> System.out.println(number));
+        ReactiveSources.intNumberMono()
+                .subscribe(
+                        number -> System.out.println(number),
+                        err -> System.out.println(err.getMessage()),
+                        () -> System.out.println("Complete"));
+
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+        Optional<Integer> result = ReactiveSources.intNumberMono().blockOptional();
+        if (result.isPresent()) {
+            System.out.println("Number is = " + result.get());
+        }
 
-        System.out.println("Press a key to end");
-        System.in.read();
+//        System.out.println("Press a key to end");
+//        System.in.read();
     }
 
 }
